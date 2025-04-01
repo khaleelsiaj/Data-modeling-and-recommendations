edd3456477490dc1abc73b412659ba63b80a4bf3
khaleel <khaleel.siaj@gmail.com>
2025-04-01 11:52:08 +0000

    `Add logging and refactored code for recommendation model`

--- recommendation_model.py
+++ recommendation_model.py
@@ -1,80 +1,142 @@
 import pandas as pd
 from db_setup import connect_to_db
 from sklearn.metrics.pairwise import cosine_similarity
+import logging
+import argparse
+
+# configuring a logger
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+# File handler
+fh = logging.FileHandler("logs/recommendation_model.log")
+fh.setLevel(logging.DEBUG)
+
+# set formatter and assign it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+fh.setFormatter(formatter)
+
+# add handler to logging
+logging.getLogger().addHandler(fh)
+
+
 # fetch the required data
+def load_data(conn):
 
+    # establish a connection with the db
+    cur = conn.cursor()
 
-conn = connect_to_db()
-cur = conn.cursor()
-def load_data():
-    cur.execute("""
+    # fetch the required data
+    cur.execute(
+        """
         SELECT i.customerid, id.stockcode, id.quantity
         FROM invoice i
         JOIN invoice_details id
         ON i.invoiceno = id.invoiceno;
-        """)
+        """
+    )
     data = cur.fetchall()
-    df = pd.DataFrame(data, columns=['customer_id', 'stock_code', 'quantity'])
 
-    df['purchased'] = 1
+    # load the data into dataframe
+    df = pd.DataFrame(data, columns=["customer_id", "stock_code", "quantity"])
+
+    # add a new purchased feature
+    df["purchased"] = 1
+
+    return df
+
 
 # add purchased column and create the pivot table
-# df['purchased'] = (df['quantity'] > 0).astype(int)
+def get_matrix(df):
+    user_item_matrix = df.pivot_table(
+        index="customer_id", columns="stock_code", values="purchased", fill_value=0
+    )
+    user_item_matrix.duplicated().sum()
+    logging.debug("Created user_item_matrix df")
+    return user_item_matrix
 
-user_item_matrix = df.pivot_table(
-    index='customer_id',
-    columns='stock_code',
-    values='purchased',
-    fill_value=0
-)
-user_item_matrix.duplicated().sum()
-user_item_matrix_T = user_item_matrix.transpose()
 
-# compute consine similarity
-item_similarity = cosine_similarity(user_item_matrix_T)
+def compute_cosine_similarity(user_item_matrix):
+    user_item_matrix_T = user_item_matrix.transpose()
 
-item_similarity_df = pd.DataFrame(
-    item_similarity,
-    index=user_item_matrix.columns,
-    columns=user_item_matrix.columns
-)
+    # compute consine similarity
+    item_similarity = cosine_similarity(user_item_matrix_T)
 
-# similar_items = item_similarity_df["10080"].sort_values(ascending=False).index[1:5]
-# d =user_item_matrix.loc[18283]
-# pd.DataFrame(d)
+    item_similarity_df = pd.DataFrame(
+        item_similarity,
+        index=user_item_matrix.columns,
+        columns=user_item_matrix.columns,
+    )
 
+    logging.debug("Calculated cosine similarity and created item_similarity_df")
+
+    return item_similarity_df
 
 
 # recommendation function
-def get_recommendations(customer_id, user_item_matrix, item_similarity, top_n=5 ):
-    
-    purchased_items = user_item_matrix.loc[customer_id][user_item_matrix.loc[customer_id] > 0].index
+def compute_recommendations(customer_id, user_item_matrix, item_similarity_df, top_n=5):
+
+    if customer_id not in user_item_matrix.index:
+        logging.error("Customer ID not found, try different one")
+        return
+
+    purchased_items = user_item_matrix.loc[customer_id][
+        user_item_matrix.loc[customer_id] > 0
+    ].index
 
     recommendations = {}
 
     for item in purchased_items:
-        similar_items = item_similarity_df.loc[item].sort_values(ascending=False)[1:top_n+1].index
+        similar_items = (
+            item_similarity_df.loc[item]
+            .sort_values(ascending=False)[1 : top_n + 1]
+            .index
+        )
         for sim_item in similar_items:
             if sim_item not in purchased_items:
                 if sim_item not in recommendations:
                     recommendations[sim_item] = item_similarity_df[item][sim_item]
                 else:
                     recommendations[sim_item] += item_similarity_df[item][sim_item]
-    
-    sorted_rec = sorted(recommendations.items(), key= lambda x:x[1], reverse=True)[:top_n]
-    return [item for item, _ in sorted_rec]
+
+    sorted_rec = sorted(recommendations.items(), key=lambda x: x[1], reverse=True)[
+        :top_n
+    ]
+    recommended_items = [item for item, _ in sorted_rec]
+    logging.debug(f"Computed top {top_n} recommended items for customer: {customer_id}")
+
+    return recommended_items, purchased_items
 
 
-recommendations = get_recommendations(18283, user_item_matrix, item_similarity)
+def show_recommendations(conn, customer_id, recommended_items, purchased_items):
+
+    cur = conn.cursor()
+
+    logging.info(f"Items bought by customer: {customer_id}")
+    for item in purchased_items:
+        cur.execute("SELECT DESCRIPTION FROM product WHERE stockcode = %s;", (item,))
+        product_name = cur.fetchone()[0]
+        logging.info(product_name)
+
+    logging.info(f"Recommended items:")
+    for item in recommended_items:
+        cur.execute("SELECT description FROM product WHERE stockcode = %s;", (item,))
+        product_name = cur.fetchone()[0]
+        logging.info(product_name)
 
 
+if __name__ == "__main__":
+    # essential variables
+    conn = connect_to_db()
+    customer_id = 12346
 
-purchased_items = user_item_matrix.loc[18283][user_item_matrix.loc[18283] > 0].index
-for item in purchased_items:
-    cur.execute('SELECT DESCRIPTION FROM product WHERE stockcode = %s;', (item,))
-    r = cur.fetchone()
-    print(r)
-for item in recommendations:
-    cur.execute('SELECT DESCRIPTION FROM product WHERE stockcode = %s;', (item,))
-    r = cur.fetchone()
-    print(r)
+    df = load_data(conn)
+    user_item_matrix = get_matrix(df)
+    item_similary_df = compute_cosine_similarity(user_item_matrix)
+    recommended_items, purchased_items = compute_recommendations(
+        customer_id,
+        user_item_matrix=user_item_matrix,
+        item_similarity_df=item_similary_df,
+    )
+    show_recommendations(conn, customer_id, recommended_items, purchased_items)
